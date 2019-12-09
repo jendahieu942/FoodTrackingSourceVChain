3204b3a8d94b60b5b471fe83900f6fe8c5ec494a
hieutv <hieutranvan@admicro.vn>
2019-12-09 14:09:44 +0700

    +fix error in menu

--- screens/Menu.js
+++ screens/Menu.js
@@ -4,6 +4,9 @@ import {View} from 'react-native'
 export default class Menu extends Compoent {
     constructor(props) {
         super(props);
+        this.state = {
+            username: 'userdefault1', password: '12345678'
+        }
     }
 
     state = {
